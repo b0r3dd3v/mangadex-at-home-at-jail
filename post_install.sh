f21ec3b7ed64b1425a7ec6b3022ef357612450d7
radonbark <radonbark@protonmail.com>
2020-06-21 13:26:42 -0400

    Move rc.d. Add user to rc.d +x rc.d

--- post_install.sh
+++ post_install.sh
@@ -3,6 +3,9 @@
 # Create user 'mangadexathome' with shell '/bin/nologin' with no home directory. For home directory, add '-m'
 pw add user -n mangadexathome -c mangadexathome -s /bin/nologin
 
+# Make are rc.d script executable
+chmod +x /usr/local/etc/rc.d/mangadexathome
+
 # Configure mangadexathome
 
 sysrc mangadexathome_enable=YES
