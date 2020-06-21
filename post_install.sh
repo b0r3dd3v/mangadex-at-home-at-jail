8b1e54622a09b07c0074bc8c2dd8e414e11e96be
radonbark <radonbark@protonmail.com>
2020-06-21 14:20:37 -0400

    Check if settings.json already exists and is outdated

--- post_install.sh
+++ post_install.sh
@@ -6,9 +6,31 @@ pw add user -n mangadexathome -c mangadexathome -s /bin/nologin
 # Make are rc.d script executable
 chmod +x /etc/rc.d/mangadexathome
 
-# Configure mangadexathome
+# Add mangadexathome as the owner of the setting.json.default
+chown mangadexathome:mangadexathome /usr/home/mangadexathome/mangadexathome/settings.json.default
 
+# Change into the main mangadexathome directory
+cd /usr/home/mangadexathome/mangadexathome
+
+# Deal with the settings.json file
+if [ -e settings.json ]
+then
+  # If "settings.json" exists, check if it is an outdated configuration
+  defaultVersion = jq '.unofficial_config_version' settings.json.default
+  operationalVersion = jq '.unofficial_config_version' settings.json
+  
+  if [ $defaultVersion -ne $operationalVersion ]
+  then
+    echo "The settings.json 'unofficial_config_version' differs from the value in settings.json.default. Please compare them for changes and update the version as needed."
+  fi
+else
+  # If "settings.json" doesn't exist, make it based on "settings.json.default"
+  cp settings.json.default settings.json
+fi
+
+# Enable mangadexathome service
 sysrc mangadexathome_enable=YES
 
-# Start the service
-#service mangadexathome start
+# We can't start the service because the settings.json has to be configured
+
+echo "Test output to user"
